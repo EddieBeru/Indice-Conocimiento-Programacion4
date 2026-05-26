a417cbbdc268358b8e3dc343faa04b53103c44dd
Edie Bermúdez <eddieberu@outlook.com>
2026-05-26 00:30:41 -0600

    Update site and base properties in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import relativeLinks from 'astro-relative-links';
 
 export default defineConfig({
-    build: {
-        format: 'file'
-    },
-    integrations: [relativeLinks()]
+  site: 'https://eddieberu.github.io/Indice-Conocimiento-Programacion4/',
+  base: '/Indice-Conocimiento-Programacion4/',
+  build: { format: 'file' },
+  integrations: [relativeLinks()]
 });
